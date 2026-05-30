0a94b051cba0fbd1e7c4b7e386fd6cddc690ba08
代码搬运工 <994301536@qq.com>
2026-05-30 15:03:25 +0800

    更新

--- build-wasm.ps1
+++ build-wasm.ps1
@@ -3,11 +3,13 @@ param(
 )
 
 $wasmPaths = @{
-    "general"          = "pkg/template_replacement_core_wasm_bg.wasm"
-    "sign"             = "pkg/template_replacement_sign_core_wasm_bg.wasm"
-    "general-polyfill" = "pkg/template_replacement_core_wasm_polyfill_polyfill_bg.wasm"
-    "sign-polyfill"    = "pkg/template_replacement_sign_core_wasm_polyfill_bg.wasm"
+    "general" = "pkg/template_replacement_core_wasm_bg.wasm"
+    "sign" = "pkg/template_replacement_sign_core_wasm_bg.wasm"
+    "general-polyfill" = "pkg/template_replacement_core_wasm_polyfill_bg.wasm"
+    "sign-polyfill" = "pkg/template_replacement_sign_core_wasm_polyfill_bg.wasm"
 }
+
+$repositoryUrl = "git+https://github.com/fushiliang321/template-replacement-core.git"
 
 if ($crate -eq $null)
 {
@@ -34,6 +36,12 @@ foreach ($crate in $crates)
         {
             exit $LASTEXITCODE
         }
+        $pkgJsonPath = Join-Path (Join-Path $PSScriptRoot $crate) "pkg/package.json"
+        $pkgJson = Get-Content $pkgJsonPath -Raw -Encoding UTF8 | ConvertFrom-Json
+        $pkgJson | Add-Member -NotePropertyName "repository" -NotePropertyValue @{ type = "git"; url = $repositoryUrl } -Force
+        $jsonText = $pkgJson | ConvertTo-Json -Depth 3
+        [System.IO.File]::WriteAllText($pkgJsonPath, $jsonText, [System.Text.UTF8Encoding]::new($false))
+        node -e "const f=require('fs'),p=process.argv[1];f.writeFileSync(p,JSON.stringify(JSON.parse(f.readFileSync(p,'utf8')),null,2)+'\n','utf8')" $pkgJsonPath
     }
     finally
     {
